7a6a2277d836a6de12bdcf3054a5cbcb7831e07c
Dadu <shaikzied878@gmail.com>
2020-10-31 19:49:34 -0700

    First commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: "Do I have to allow the use of cookes?",
+    info:
+      "Yes, to get all feauters in the website you need to accept all the cookies. If you are a temporary user, you know what I mean. If you just wanna see that website for once and you will never visit it again then you can ignore them.",
+  },
+  {
+    id: 2,
+    title: "How do I change my My Page password?",
+    info:
+      "In every Website, While Logging-in. you see a Option forgot-password. If you dont Remember the password, you can click on that and it will send you a otp or password changing link or some-things. Every Website use Some Type of Things. You can Follow their Steps",
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    info:
+      "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+  },
+  {
+    id: 4,
+    title: "Whose birth number can I use?",
+    info:
+      "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+  },
+  {
+    id: 5,
+    title: "When do I recieve a password ordered by letter?",
+    info:
+      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+  },
+];
+export default questions;
